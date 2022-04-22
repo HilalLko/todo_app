66cb84a53ab297a9a25d92c8c3acdaa91d980306
HILAL RAUF <hilal@hipster-inc.com>
2022-04-22 10:29:04 +0530

    first commit

--- database/seeders/MenusTableSeeder.php
+++ database/seeders/MenusTableSeeder.php
@@ -1,11 +1,8 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-use Illuminate\Support\Facades\DB;
 
 class MenusTableSeeder extends Seeder
 {
@@ -14,6 +11,7 @@ class MenusTableSeeder extends Seeder
     private $dropdown = false;
     private $sequence = 1;
     private $joinData = array();
+    private $superRole = null;
     private $adminRole = null;
     private $userRole = null;
     private $subFolder = '';
@@ -76,6 +74,9 @@ class MenusTableSeeder extends Seeder
         if(in_array('admin', $roles)){
             $this->adminRole->givePermissionTo($permission);
         }
+        if(in_array('super', $roles)){
+            $this->adminRole->givePermissionTo($permission);
+        }
         return $lastId;
     }
 
@@ -127,6 +128,7 @@ class MenusTableSeeder extends Seeder
     public function run()
     { 
         /* Get roles */
+        $this->superRole = Role::where('name' , '=' , 'super' )->first();
         $this->adminRole = Role::where('name' , '=' , 'admin' )->first();
         $this->userRole = Role::where('name', '=', 'user' )->first();
         /* Create Sidebar menu */
@@ -134,68 +136,23 @@ class MenusTableSeeder extends Seeder
             'name' => 'sidebar menu'
         ]);
         $this->menuId = DB::getPdo()->lastInsertId();  //set menuId
-        $this->insertLink('guest,user,admin', 'Dashboard', '/', 'cil-speedometer');
-        $this->beginDropdown('admin', 'Settings', 'cil-calculator');
-            $this->insertLink('admin', 'Notes',                   '/notes');
-            $this->insertLink('admin', 'Users',                   '/users');
-            $this->insertLink('admin', 'Edit menu',               '/menu/menu');
-            $this->insertLink('admin', 'Edit menu elements',      '/menu/element');
-            $this->insertLink('admin', 'Edit roles',              '/roles');
-            $this->insertLink('admin', 'Media',                   '/media');
-            $this->insertLink('admin', 'BREAD',                   '/bread');
-            $this->insertLink('admin', 'Email',                   '/mail');
+        $this->insertLink('user,admin,super', 'Dashboard', '/sitemaster/', 'cil-speedometer');
+        $this->beginDropdown('admin,super', 'User Management', 'cil-user');    
+            $this->insertLink('super,admin', 'Partially Registered','/sitemaster/partial-users');
+            $this->insertLink('super,admin', 'Review Request','/sitemaster/review-requests');
+            $this->insertLink('super,admin', 'Approved Registered','/sitemaster/users');
         $this->endDropdown();
-        $this->insertLink('guest', 'Login', '/login', 'cil-account-logout');
-        $this->insertLink('guest', 'Register', '/register', 'cil-account-logout');
-        $this->insertTitle('user,admin', 'Theme');
-        $this->insertLink('user,admin', 'Colors', '/colors', 'cil-drop1');
-        $this->insertLink('user,admin', 'Typography', '/typography', 'cil-pencil');
-        $this->beginDropdown('user,admin', 'Base', 'cil-puzzle');
-            $this->insertLink('user,admin', 'Breadcrumb',    '/base/breadcrumb');
-            $this->insertLink('user,admin', 'Cards',         '/base/cards');
-            $this->insertLink('user,admin', 'Carousel',      '/base/carousel');
-            $this->insertLink('user,admin', 'Collapse',      '/base/collapse');
-            $this->insertLink('user,admin', 'Forms',         '/base/forms');
-            $this->insertLink('user,admin', 'Jumbotron',     '/base/jumbotron');
-            $this->insertLink('user,admin', 'List group',    '/base/list-group');
-            $this->insertLink('user,admin', 'Navs',          '/base/navs');
-            $this->insertLink('user,admin', 'Pagination',    '/base/pagination');
-            $this->insertLink('user,admin', 'Popovers',      '/base/popovers');
-            $this->insertLink('user,admin', 'Progress',      '/base/progress');
-            $this->insertLink('user,admin', 'Scrollspy',     '/base/scrollspy');
-            $this->insertLink('user,admin', 'Switches',      '/base/switches');
-            $this->insertLink('user,admin', 'Tables',        '/base/tables');
-            $this->insertLink('user,admin', 'Tabs',          '/base/tabs');
-            $this->insertLink('user,admin', 'Tooltips',      '/base/tooltips');
+        
+        
+        $this->beginDropdown('admin,super', 'Settings', 'cil-calculator');    
+            $this->insertLink('super', 'Edit menu',               '/sitemaster/menu/menu');
+            $this->insertLink('super', 'Edit menu elements',      '/sitemaster/menu/element');
+            $this->insertLink('admin,super', 'Edit roles',              '/sitemaster/roles');
+            $this->insertLink('admin,super', 'Media',                   '/sitemaster/media');
+            $this->insertLink('admin,super', 'BREAD',                   '/sitemaster/bread');
+            $this->insertLink('admin,super', 'Email',                   '/sitemaster/mail');
         $this->endDropdown();
-            $this->beginDropdown('user,admin', 'Buttons', 'cil-cursor');
-            $this->insertLink('user,admin', 'Buttons',           '/buttons/buttons');
-            $this->insertLink('user,admin', 'Buttons Group',     '/buttons/button-group');
-            $this->insertLink('user,admin', 'Dropdowns',         '/buttons/dropdowns');
-            $this->insertLink('user,admin', 'Brand Buttons',     '/buttons/brand-buttons');
-        $this->endDropdown();
-        $this->insertLink('user,admin', 'Charts', '/charts', 'cil-chart-pie');
-        $this->beginDropdown('user,admin', 'Icons', 'cil-star');
-            $this->insertLink('user,admin', 'CoreUI Icons',      '/icon/coreui-icons');
-            $this->insertLink('user,admin', 'Flags',             '/icon/flags');
-            $this->insertLink('user,admin', 'Brands',            '/icon/brands');
-        $this->endDropdown();
-        $this->beginDropdown('user,admin', 'Notifications', 'cil-bell');
-            $this->insertLink('user,admin', 'Alerts',     '/notifications/alerts');
-            $this->insertLink('user,admin', 'Badge',      '/notifications/badge');
-            $this->insertLink('user,admin', 'Modals',     '/notifications/modals');
-        $this->endDropdown();
-        $this->insertLink('user,admin', 'Widgets', '/widgets', 'cil-calculator');
-        $this->insertTitle('user,admin', 'Extras');
-        $this->beginDropdown('user,admin', 'Pages', 'cil-star');
-            $this->insertLink('user,admin', 'Login',         '/login');
-            $this->insertLink('user,admin', 'Register',      '/register');
-            $this->insertLink('user,admin', 'Error 404',     '/404');
-            $this->insertLink('user,admin', 'Error 500',     '/500');
-        $this->endDropdown();
-        $this->insertLink('guest,user,admin', 'Download CoreUI', 'https://coreui.io', 'cil-cloud-download');
-        $this->insertLink('guest,user,admin', 'Try CoreUI PRO', 'https://coreui.io/pro/', 'cil-layers');
-
+        $this->insertLink('guest', 'Login', '/sitemaster/login', 'cil-account-logout');
 
         /* Create top menu */
         DB::table('menulist')->insert([
@@ -204,16 +161,16 @@ class MenusTableSeeder extends Seeder
         $this->menuId = DB::getPdo()->lastInsertId();  //set menuId
         $this->beginDropdown('guest,user,admin', 'Pages');
         $id = $this->insertLink('guest,user,admin', 'Dashboard',    '/');
-        $id = $this->insertLink('user,admin', 'Notes',              '/notes');
-        $id = $this->insertLink('admin', 'Users',                   '/users');
+        $id = $this->insertLink('user,admin,super', 'Notes',              '/sitemaster/notes');
+        $id = $this->insertLink('admin,super', 'Users',                   '/sitemaster/users');
         $this->endDropdown();
-        $id = $this->beginDropdown('admin', 'Settings');
+        $id = $this->beginDropdown('admin,super', 'Settings');
 
-        $id = $this->insertLink('admin', 'Edit menu',               '/menu/menu');
-        $id = $this->insertLink('admin', 'Edit menu elements',      '/menu/element');
-        $id = $this->insertLink('admin', 'Edit roles',              '/roles');
-        $id = $this->insertLink('admin', 'Media',                   '/media');
-        $id = $this->insertLink('admin', 'BREAD',                   '/bread');
+        $id = $this->insertLink('super', 'Edit menu',               '/sitemaster/menu/menu');
+        $id = $this->insertLink('super', 'Edit menu elements',      '/sitemaster/menu/element');
+        $id = $this->insertLink('super', 'Edit roles',              '/sitemaster/roles');
+        $id = $this->insertLink('admin/super', 'Media',                   '/sitemaster/media');
+        $id = $this->insertLink('super', 'BREAD',                   '/sitemaster/bread');
         $this->endDropdown();
 
         $this->joinAllByTransaction(); ///   <===== Must by use on end of this seeder
--- database/seeders/UsersAndNotesSeeder.php
+++ database/seeders/UsersAndNotesSeeder.php
@@ -19,26 +19,26 @@ class UsersAndNotesSeeder extends Seeder
      */
     public function run()
     {
-        $numberOfUsers = 10;
-        $numberOfNotes = 100;
-        $usersIds = array();
-        $statusIds = array();
-        $faker = Faker::create();
         /* Create roles */
+        $superRole = Role::create(['name' => 'super']); 
+        RoleHierarchy::create([
+            'role_id' => $superRole->id,
+            'hierarchy' => 1,
+        ]);
         $adminRole = Role::create(['name' => 'admin']); 
         RoleHierarchy::create([
             'role_id' => $adminRole->id,
-            'hierarchy' => 1,
+            'hierarchy' => 2,
         ]);
         $userRole = Role::create(['name' => 'user']);
         RoleHierarchy::create([
             'role_id' => $userRole->id,
-            'hierarchy' => 2,
+            'hierarchy' => 3,
         ]);
         $guestRole = Role::create(['name' => 'guest']); 
         RoleHierarchy::create([
             'role_id' => $guestRole->id,
-            'hierarchy' => 3,
+            'hierarchy' => 4,
         ]);
         
         /*  insert status  */
@@ -46,59 +46,39 @@ class UsersAndNotesSeeder extends Seeder
             'name' => 'ongoing',
             'class' => 'badge badge-pill badge-primary',
         ]);
-        array_push($statusIds, DB::getPdo()->lastInsertId());
         DB::table('status')->insert([
             'name' => 'stopped',
             'class' => 'badge badge-pill badge-secondary',
         ]);
-        array_push($statusIds, DB::getPdo()->lastInsertId());
         DB::table('status')->insert([
             'name' => 'completed',
             'class' => 'badge badge-pill badge-success',
         ]);
-        array_push($statusIds, DB::getPdo()->lastInsertId());
         DB::table('status')->insert([
             'name' => 'expired',
             'class' => 'badge badge-pill badge-warning',
         ]);
-        array_push($statusIds, DB::getPdo()->lastInsertId());
         /*  insert users   */
+        $super = User::create([ 
+            'name' => 'admin',
+            'email' => 'hilal_rf+super@hotmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('Super@123$'), // password
+            'remember_token' => Str::random(10),
+            'menuroles' => 'super,admin,user' 
+        ]);
+        $super->assignRole('super');
+        $super->assignRole('admin');
+        $super->assignRole('user');
         $user = User::create([ 
             'name' => 'admin',
-            'email' => 'admin@admin.com',
+            'email' => 'hilal_rf+admin@hotmail.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('Admin@123$'), // password
             'remember_token' => Str::random(10),
             'menuroles' => 'user,admin' 
         ]);
         $user->assignRole('admin');
         $user->assignRole('user');
-        for($i = 0; $i<$numberOfUsers; $i++){
-            $user = User::create([ 
-                'name' => $faker->name(),
-                'email' => $faker->unique()->safeEmail(),
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-                'menuroles' => 'user'
-            ]);
-            $user->assignRole('user');
-            array_push($usersIds, $user->id);
-        }
-        /*  insert notes  */
-        for($i = 0; $i<$numberOfNotes; $i++){
-            $noteType = $faker->word();
-            if(random_int(0,1)){
-                $noteType .= ' ' . $faker->word();
-            }
-            DB::table('notes')->insert([
-                'title'         => $faker->sentence(4,true),
-                'content'       => $faker->paragraph(3,true),
-                'status_id'     => $statusIds[random_int(0,count($statusIds) - 1)],
-                'note_type'     => $noteType,
-                'applies_to_date' => $faker->date(),
-                'users_id'      => $usersIds[random_int(0,$numberOfUsers-1)]
-            ]);
-        }
     }
 }
