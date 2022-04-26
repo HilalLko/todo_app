b49d6fee67508294bd0c9b1034a24bf99206d38c
HILAL RAUF <hilal@hipster-inc.com>
2022-04-26 14:48:15 +0530

    Fixing Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\admin\ActivitiesController;
+use App\Http\Controllers\admin\ResourceController;
+use App\Http\Controllers\admin\UsersController;
+use App\Http\Controllers\admin\BreadController;
+use App\Http\Controllers\admin\RolesController;
+use App\Http\Controllers\admin\MailController;
+use App\Http\Controllers\admin\MenuController;
+use App\Http\Controllers\admin\MenuElementController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,14 +23,14 @@
 */
 Route::get('/', function () {           return view('welcome'); });
 Route::prefix('sitemaster')->group(function () {
-    Route::get('/', 'Auth\LoginController@showLoginForm');
+    Route::get('/', [LoginController::class, 'showLoginForm']);
     Auth::routes();
     Route::group(['middleware' => ['auth','get.menu']], function () {
-        Route::get('/dashboard','admin\ActivitiesController@index')->name('admin.dashboard');
-        Route::get('/global-activities','admin\ActivitiesController@getGlobalActivities')->name('admin.global_activities');
-        Route::post('/global-activities','admin\ActivitiesController@addGlobalActivity')->name('admin.activity.add');
-        Route::get('/user-activities','admin\ActivitiesController@getUserActivities')->name('admin.global_activities');
-        Route::resource('resource/{table}/resource', 'admin\ResourceController')->names([
+        Route::get('/dashboard',[ActivitiesController::class, 'index'])->name('admin.dashboard');
+        Route::get('/global-activities',[ActivitiesController::class, 'getGlobalActivities'])->name('admin.global_activities');
+        Route::post('/global-activities',[ActivitiesController::class, 'addGlobalActivity'])->name('admin.activity.add');
+        Route::get('/user-activities',[ActivitiesController::class, 'getUserActivities'])->name('admin.global_activities');
+        Route::resource('resource/{table}/resource', ResourceController::class)->names([
             'index'     => 'resource.index',
             'create'    => 'resource.create',
             'store'     => 'resource.store',
@@ -28,35 +39,35 @@ Route::prefix('sitemaster')->group(function () {
             'update'    => 'resource.update',
             'destroy'   => 'resource.destroy'
         ]);
-        Route::get('/admins','admin\UsersController@getAdminUsers')->name('admin.admins');        
+        Route::get('/admins',[UsersController::class, 'getAdminUsers'])->name('admin.admins');        
         Route::group(['middleware' => ['role:super']], function () {
-            Route::resource('bread',  'admin\BreadController');   //create BREAD (resource)
-            Route::resource('users',        'admin\UsersController')->except( ['create', 'store'] );
-            Route::resource('roles',        'admin\RolesController');
-            Route::resource('mail',        'admin\MailController');
-            Route::get('prepareSend/{id}',        'admin\MailController@prepareSend')->name('prepareSend');
-            Route::post('mailSend/{id}',        'admin\MailController@send')->name('mailSend');
-            Route::get('/roles/move/move-up',      'admin\RolesController@moveUp')->name('roles.up');
-            Route::get('/roles/move/move-down',    'admin\RolesController@moveDown')->name('roles.down');
+            Route::resource('bread', BreadController::class);
+            Route::resource('users', UsersController::class)->except( ['create', 'store'] );
+            Route::resource('roles', RolesController::class);
+            Route::resource('mail',  MailController::class);
+            Route::get('prepareSend/{id}', [MailController::class, 'prepareSend'])->name('prepareSend');
+            Route::post('mailSend/{id}', [MailController::class, 'send'])->name('mailSend');
+            Route::get('/roles/move/move-up', [RolesController::class, 'moveUp'])->name('roles.up');
+            Route::get('/roles/move/move-down', [RolesController::class, 'moveDown'])->name('roles.down');
             Route::prefix('menu/element')->group(function () { 
-                Route::get('/',             'admin\MenuElementController@index')->name('menu.index');
-                Route::get('/move-up',      'admin\MenuElementController@moveUp')->name('menu.up');
-                Route::get('/move-down',    'admin\MenuElementController@moveDown')->name('menu.down');
-                Route::get('/create',       'admin\MenuElementController@create')->name('menu.create');
-                Route::post('/store',       'admin\MenuElementController@store')->name('menu.store');
-                Route::get('/get-parents',  'admin\MenuElementController@getParents')->name('menu.parent');
-                Route::get('/edit',         'admin\MenuElementController@edit')->name('menu.edit');
-                Route::post('/update',      'admin\MenuElementController@update')->name('menu.update');
-                Route::get('/show',         'admin\MenuElementController@show')->name('menu.show');
-                Route::get('/delete',       'admin\MenuElementController@delete')->name('menu.delete');
+                Route::get('/',             [MenuElementController::class, 'index'])->name('menu.index');
+                Route::get('/move-up',      [MenuElementController::class, 'moveUp'])->name('menu.up');
+                Route::get('/move-down',    [MenuElementController::class, 'moveDown'])->name('menu.down');
+                Route::get('/create',       [MenuElementController::class, 'create'])->name('menu.create');
+                Route::post('/store',       [MenuElementController::class, 'store'])->name('menu.store');
+                Route::get('/get-parents',  [MenuElementController::class, 'getParents'])->name('menu.parent');
+                Route::get('/edit',         [MenuElementController::class, 'edit'])->name('menu.edit');
+                Route::post('/update',      [MenuElementController::class, 'update'])->name('menu.update');
+                Route::get('/show',         [MenuElementController::class, 'show'])->name('menu.show');
+                Route::get('/delete',       [MenuElementController::class, 'delete'])->name('menu.delete');
             });
             Route::prefix('menu/menu')->group(function () { 
-                Route::get('/',         'admin\MenuController@index')->name('menu.menu.index');
-                Route::get('/create',   'admin\MenuController@create')->name('menu.menu.create');
-                Route::post('/store',   'admin\MenuController@store')->name('menu.menu.store');
-                Route::get('/edit',     'admin\MenuController@edit')->name('menu.menu.edit');
-                Route::post('/update',  'admin\MenuController@update')->name('menu.menu.update');
-                Route::get('/delete',   'admin\MenuController@delete')->name('menu.menu.delete');
+                Route::get('/',         [MenuController::class, 'index'])->name('menu.menu.index');
+                Route::get('/create',   [MenuController::class, 'create'])->name('menu.menu.create');
+                Route::post('/store',   [MenuController::class, 'store'])->name('menu.menu.store');
+                Route::get('/edit',     [MenuController::class, 'edit'])->name('menu.menu.edit');
+                Route::post('/update',  [MenuController::class, 'update'])->name('menu.menu.update');
+                Route::get('/delete',   [MenuController::class, 'delete'])->name('menu.menu.delete');
             });
         });
     });
